a5688ee54147dfbe6aa842d1f4bbf27388bb2ffb
ndf-zz <ndf-zz@6-v.org>
2024-05-08 15:59:44 +1000

    Start midi-usb device

--- README.md
+++ README.md
@@ -4,23 +4,12 @@ MIDI/USB to DIN sync converter
 
 ![Prototype](prototype.jpg "Prototype")
 
-[in progress]
+[work in progress]
 
 ## Contents
 
    - pcb: Kicad schematic and circuit board
    - enclosure: FreeCAD enclosure
    - firmware: STM32 firmware
-
-## Firmware installation
-
-From the firmware folder, install development environment
-(gcc, binutils, openocd & gdb) using make:
-
-	$ make requires
-
-Build the combined firmware and fash loader, then upload
-to attached target via stlink/SWD:
-
-	$ make upload
+   - syncbox-edit: Python SysEx configuration editor
 
--- firmware/src/midi_usb.c
+++ firmware/src/midi_usb.c
@@ -4,15 +4,22 @@
  * USB-MIDI Class Device Interface
  */
 #include "stm32f303xe.h"
+#include "flash.h"
+#include "settings.h"
+#include "usb.h"
 #include "midi_usb.h"
 
 // Initialise hardware and enable interrupts
 void midi_usb_init(void)
 {
-        // Force USB re-enumeration
-        GPIOA->BSRR = GPIO_BSRR_BR_12;
-        uint32_t nm = GPIOA->MODER & ~GPIO_MODER_MODER12_Msk;
-        GPIOA->MODER = nm | (0x1 << GPIO_MODER_MODER12_Pos);
-        delay_ms(6);
-        GPIOA->MODER = nm | (0x3 << GPIO_MODER_MODER12_Pos);
+	// Check for a sane-ish USB config
+	if (OPTION->usb.device.bLength == USB_DEVLEN
+	    && OPTION->usb.device.bDescriptorType == USB_DEVICE) {
+		// Force USB re-enumeration
+		GPIOA->BSRR = GPIO_BSRR_BR_12;
+		uint32_t nm = GPIOA->MODER & ~GPIO_MODER_MODER12_Msk;
+		GPIOA->MODER = nm | (0x1 << GPIO_MODER_MODER12_Pos);
+		delay_ms(6);
+		GPIOA->MODER = nm | (0x3 << GPIO_MODER_MODER12_Pos);
+	}
 }
